5089336fa36c6a31ab59413b60ed4792fa80cfa3
Rebecca Njagi <rebeccanjagi@gmail.com>
2016-04-27 18:33:41 +0300

    Importer

---
+++ day_3/importer.py
@@ -0,0 +1,15 @@
+from exporter import super_sum, hello_again, max_min, people
+
+print super_sum, hello_again, max_min, people
+
+a = [10,45,78,-9]
+
+print hello_again('Jack', 89)
+
+print max_min(a)
+
+print hello_again(*people[0])
+
+print super_sum(*a)
+
+print max_min(a)
