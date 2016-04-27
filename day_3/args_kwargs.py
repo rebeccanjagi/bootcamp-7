619fe19144833b278935b0449612c4481206fb4c
Rebecca Njagi <rebeccanjagi@gmail.com>
2016-04-27 18:12:20 +0300

    Args-Kwargs

--- day_3/args_kwargs.py
+++ day_3/args_kwargs.py
@@ -1,38 +1,64 @@
-def helloA(name, age):
+# unpacking
+def hello(name, age, class_=''):
+    '''
+    A function that tests the format function and outputs
+    '''
+    if class_ != '':
+        return "I am {}, {} y/o, and {} class".format(name, age, class_)
+    return "I am {}, and I'm {}".format(name, age)
 
-	print "I am {} person and am {} old".format(name,age)
 
-people = [("Jane", 23), ("Joe", 25), ("Brian",60), ("Betty",45)]
+people = [
+            ("Jane", 23, 'High'),
+            ("Joe", 25, 'Low'),
+            ("Brian", 60),
+            ("Betty", 45)
+         ]
 
 
+# unpacking
+
+for person in people:
+    print hello(*person)
 
 
+def super_sum(a, b, *args): #args is not a reserved word. takes in a tuple
+    '''
+    Takes in variable number of items,
+    and returns the sum.
+    e.g. super_sum(10, 20) = 30
+        super_sum(10, 20, 40) = 70
+        super_sum(1, 4, 5, 6, 7) = 23
+    '''
+    total = 0
+    for i in args:
+        total += i
 
-def super_sum(*args):
-	'''
-	Takes in variable number of items,
-	and returns the sum.
-	e.g super_sum(10,20) = 30
-		super_sum(10, 20, 40) = 70
+    return total + a + b
 
-	
-	total = 0
-	for i in args:
-		total += i
-	return total
-'''
-#
-#print super_sum(10, 40, 60)
+print super_sum(10, 20)
+print super_sum(1, 4, 5, 7)
+a = [10, 40, 60]
+print super_sum(*a)
+print super_sum(10, 20)
 
-#a = 10, 40, 60
-#print super_sum(*a)
+def hello_again(**kwargs): #kwargs is not a reserved word. Takes in a dictionary
+    return "I am {}, and I'm {}".format(kwargs['name'], kwargs['age'])
 
-def hello_again(**kwargs):
-	print "I am {}, and I am {}".format(kwargs['name'],kwargs['age'])
 
-hello_again(name='becks', age=23)
+print hello_again(name='Joe', age=20)
+print hello_again(age=20, name='Jane')
 
-joe ={'name':'joe', 'age':25}
-hello_again(**joe)
-hello_again(name='joe', age=23)
+joe = {'name': 'Joe', 'age': 98}
 
+print hello_again(**joe)
+print hello_again(name='Joe', age=98)
+
+other_people = [
+        {'name': 'Joe', 'age': 98},
+        {'name': 'Jane', 'age': 60},
+        {'name': 'Trump', 'age': 23}
+    ]
+
+for person in other_people:
+    hello_again(**person)
