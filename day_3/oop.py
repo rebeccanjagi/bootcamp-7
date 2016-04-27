1da144c94391dfe7d625dd9baae932507850ff12
Rebecca Njagi <rebeccanjagi@gmail.com>
2016-04-27 17:48:22 +0300

    OOP - tests imports

--- day_3/oop.py
+++ day_3/oop.py
@@ -1,6 +1,7 @@
 from person import Person
 from kenyan import Kenyan
 
+#tests using imports from person module
 p = Person('Becks',2)
 p2 = Person('Lorna',30)
 p2 = Person('Lot', 20)
@@ -11,7 +12,7 @@ a=[('Jane',23), ('Joe',56), ('Lot',26), ('Simon',44), ('Gog',16), ('Ken',6)]
 b=[]
 
 for name, age in a:
-	person =Person(name, age)
+	person = Person(name, age)
 	b.append(person)
 
 print b
@@ -19,7 +20,7 @@ print b
 for w in b:
 	print w.say_hello()
 
-
+#tests using imports from kenyan module
 
 k = Kenyan('Jane', 20)
 
